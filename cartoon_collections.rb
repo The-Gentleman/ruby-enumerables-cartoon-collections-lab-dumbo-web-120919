6fcbfde0b093b783e95cbda36ba5a89f4e30f825
Zeshan Raja <zraja4402@gmail.com>
2019-12-04 21:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls(words_array)
   words_array.each do |string|
    if string.length <= 4
      return false 
-   if string.length > 4 
+   end    
+   if  string.length > 4 
      return true 
    end   
   end 
